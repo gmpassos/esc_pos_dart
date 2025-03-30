cab503b563713d934f59ce4579f92febf4741b3e
gmpassos <gracilianomp@gmail.com>
2025-03-30 18:22:39 -0300

    - Converted to Dart enums:   - `PaperSize`, `PosTextSize`, `PosBeepDuration`.

--- lib/src/utils/enums.dart
+++ lib/src/utils/enums.dart
@@ -19,41 +19,46 @@ enum PosDrawer { pin2, pin5 }
 /// graphics: GS ( L
 enum PosImageFn { bitImageRaster, graphics }
 
-class PosTextSize {
-  const PosTextSize._internal(this.value);
+enum PosTextSize {
+  size1(1),
+  size2(2),
+  size3(3),
+  size4(4),
+  size5(5),
+  size6(6),
+  size7(7),
+  size8(8);
+
   final int value;
-  static const size1 = PosTextSize._internal(1);
-  static const size2 = PosTextSize._internal(2);
-  static const size3 = PosTextSize._internal(3);
-  static const size4 = PosTextSize._internal(4);
-  static const size5 = PosTextSize._internal(5);
-  static const size6 = PosTextSize._internal(6);
-  static const size7 = PosTextSize._internal(7);
-  static const size8 = PosTextSize._internal(8);
+  const PosTextSize(this.value);
 
   static int decSize(PosTextSize height, PosTextSize width) =>
       16 * (width.value - 1) + (height.value - 1);
 }
 
-class PaperSize {
-  const PaperSize._internal(this.value);
+enum PaperSize {
+  mm58(1),
+  mm80(2);
+
   final int value;
-  static const mm58 = PaperSize._internal(1);
-  static const mm80 = PaperSize._internal(2);
+
+  const PaperSize(this.value);
 
   int get width => value == PaperSize.mm58.value ? 372 : 558;
 }
 
-class PosBeepDuration {
-  const PosBeepDuration._internal(this.value);
+enum PosBeepDuration {
+  beep50ms(1),
+  beep100ms(2),
+  beep150ms(3),
+  beep200ms(4),
+  beep250ms(5),
+  beep300ms(6),
+  beep350ms(7),
+  beep400ms(8),
+  beep450ms(9);
+
   final int value;
-  static const beep50ms = PosBeepDuration._internal(1);
-  static const beep100ms = PosBeepDuration._internal(2);
-  static const beep150ms = PosBeepDuration._internal(3);
-  static const beep200ms = PosBeepDuration._internal(4);
-  static const beep250ms = PosBeepDuration._internal(5);
-  static const beep300ms = PosBeepDuration._internal(6);
-  static const beep350ms = PosBeepDuration._internal(7);
-  static const beep400ms = PosBeepDuration._internal(8);
-  static const beep450ms = PosBeepDuration._internal(9);
+
+  const PosBeepDuration(this.value);
 }
